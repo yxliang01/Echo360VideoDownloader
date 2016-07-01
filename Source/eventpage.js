350bd0270dff64e0aded57477c017935253e588b
yxliang01 <13267.okk@gmail.com>
2016-07-01 06:15:42 +0200

    Something

--- Source/eventpage.js
+++ Source/eventpage.js
@@ -6,6 +6,23 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
 
 function donwloadVideo(message) {
-        console.info('Message ' + JSON.stringify(message) + '	start downloading');
-        chrome.downloads.download(message);
+    console.info('Message ' + JSON.stringify(message) + '	start downloading');
+    chrome.downloads.download(message);
 }
+
+
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { urlContains: 'g' },
+                })
+            ],
+            // And shows the extension's page action.
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
