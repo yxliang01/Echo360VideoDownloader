f6d734a4547eab7ba0cf552ffb149c351c66353e
yxliang01 <13267.okk@gmail.com>
2016-07-06 10:12:31 +0200

    Update

---
+++ Source/browser_popup.js
@@ -0,0 +1,52 @@
+'use strict';
+
+$(document).ready(function() {
+    $("button#btn_download").click(function() {
+
+        chrome.tabs.query({
+            active: true
+        }, function(tabs) {
+
+            if (tabs[0].url.indexOf("https://app.lms.unimelb.edu.au/webapps/blackboard/content/contentWrapper.jsp") != -1) {
+
+                if (tabs[0].status != "complete") {
+                    $("#status").text("This page is still loading, please click the download button later.");
+                    $("#status").addClass('invalid');
+                } else {
+                    $("#status").text("Start downloading~");
+                    $("#status").addClass('successful');
+                    DownloadVideos();
+                }
+
+            } else {
+                $("#status").text("This is not Unimelb recording download page!");
+                $("#status").addClass('invalid');
+            }
+
+        });
+    });
+    console.log();
+});
+
+
+function DownloadVideos() {
+
+    injectScript();
+    $("#status").text("Downloading~");
+    $("#status").addClass('successful');
+
+}
+
+
+
+function injectScript() {
+    chrome.tabs.executeScript(null, {
+        file: "bower_components/jquery/dist/jquery.min.js",
+        allFrames: false
+    });
+
+    chrome.tabs.executeScript(null, {
+        file: "downloadecho360video.js",
+        allFrames: false
+    });
+}
