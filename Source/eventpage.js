5417cfc6fa73a166df805241db4899d67af91c11
DavidTC <taichengz@student.unimelb.edu.au>
2017-07-19 18:40:19 +1000

    Fixed the icon changing feature

--- Source/eventpage.js
+++ Source/eventpage.js
@@ -1,8 +1,3 @@
-'use strict';
-
-var currentDT = null;
-
-/* jshint unused:vars */
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.url && message.filename) {
         console.log("Starts downloading 1 video");
@@ -14,6 +9,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
 
 function donwloadVideo(message) {
+    // console.info('Message ' + JSON.stringify(message) + '	start downloading');
     chrome.downloads.download(message, whenDownloadStatusChanged);
 }
 
@@ -21,54 +17,62 @@ function whenDownloadStatusChanged(downloadId) {
 
     if (downloadId === undefined) {
         var NotificationOptions = {
-            title: "There's recording failed to be downloaded",
-            message: "You may want to download it again by using me again",
+            title: "there's recording failed to be downloaded",
+            message: "you may want to download it again by using me again",
             isClickable: false,
-            requireInteraction: true,
-            type: "basic",
-            iconUrl: "icon128_unimelb.png"
+            requireInteraction: true
         };
 
         chrome.notifications.create("one download failed", NotificationOptions);
-
-    } else {
-        chrome.downloads.search({
-            id: downloadId
-        }, function CheckDownlaod(DIs) {
-            if (DIs.State === "complete") {
-
-                currentDT.num_completed++;
-                if (currentDT.num_completed == currentDT.num_files) {
-                    var NotificationOptions = {
-                        title: "All recordings are completely downloaded",
-                        message: "Click here to open the download folder",
-                        isClickable: true,
-                        requireInteraction: true,
-                        type: "basic",
-                        iconUrl: "icon128.png"
-                    };
-
-                    chrome.notifications.create("download complete", NotificationOptions);
-                }
-            }
-        });
     }
 
 }
 
 
-// function DownloadTask(num_files, url) {
+function DownloadTask(num_files) {
+    this.num_files = num_files;
+    this.num_completed = 0;
+}
 
-//     this.num_files = num_files;
-//     this.num_completed = 0;
-//     this.url = url;
-//     this.size = "loading";
-//     $.ajax({
-//         type: "HEAD",
-//         async: true,
-//         url: url
-//     }).done(function whenFileSizeLoaded(data, status, xhr) {
-//     	//TODO: dunno how to make a constructor function
-//         xhr.getResponseHeader('Content-Length');
-//     });
-// }
+// Icon Changing feature
+
+var anu_icon = 
+{
+    conditions : [
+    new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {urlContains: "anu"}
+    })
+    ],
+    actions    : [ new chrome.declarativeContent.SetIcon({
+        path : {"128": "./icon128_anu.png"}
+    })] 
+};
+
+var monash_icon = 
+{
+    conditions : [
+    new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {urlContains: "monash"}
+    })
+    ],
+    actions    : [ new chrome.declarativeContent.SetIcon({
+        path : {"128" : "./icon128_monash.png"}
+    }) ]
+};
+
+var rmit_icon = 
+{
+    conditions : [
+    new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {urlContains: "rmit"}
+    })
+    ],
+    actions    : [ new chrome.declarativeContent.SetIcon({path : {"128" : "./icon128_rmit.png"} }) ] 
+};
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        // Change the icon according to the domain name
+        chrome.declarativeContent.onPageChanged.addRules([anu_icon, monash_icon, rmit_icon]);
+    });
+});
